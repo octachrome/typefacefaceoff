e8362ee5a2bfb4e6c63b3f64557fa54ac02ef761
Chris Brown <chris.brown@i-nexus.com>
2013-08-19 12:51:22 +0100

    Added css to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ncftpput -f thebrown.net -R public_html/faceoff index.html model.js phrases.js libs
+ncftpput -f thebrown.net -R public_html/faceoff index.html model.js phrases.js tffo.css libs
